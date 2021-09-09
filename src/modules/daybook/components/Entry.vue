77cdb48a9a05ba14accc0bb99e52b2cfd22a6051
Alex Paredes <alexparedesr95@gmail.com>
2021-09-09 00:51:56 -0500

    FEAT: Se crea el componente Entry.

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+      <!-- Título -->
+      <div class="entry-title d-flex">
+          <span>09</span>
+          <span>Septiembre</span>
+          <span>2021, jueves</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
