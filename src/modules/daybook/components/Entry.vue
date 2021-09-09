e4e69e07cf7a733d6a6a76ca90a30ee20cc4fb26
Alex Paredes <alexparedesr95@gmail.com>
2021-09-09 01:03:28 -0500

    FEAT: Se añaden clases al componente Entry.

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -2,9 +2,13 @@
   <div class="entry-container mb-3 pointer p-2">
       <!-- Título -->
       <div class="entry-title d-flex">
-          <span>09</span>
-          <span>Septiembre</span>
-          <span>2021, jueves</span>
+          <span class="text-success fs-5 fw-bold">09</span>
+          <span class="mx-1 fs-5">Septiembre</span>
+          <span class="mx-2 fw-light">2021, jueves</span>
+      </div>
+
+      <div class="entry-description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, possimus molestiae. Eius asperiores ex veniam! Magni, facilis tempora molestias modi temporibus perspiciatis expedita nesciunt, cumque nam placeat deleniti! Veniam, a.
       </div>
   </div>
 </template>
@@ -15,6 +19,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .entry-container {
+        border-bottom: 1px solid #2c3e50;
+        transition: 0.2s all ease-in;
 
+        &:hover {
+            background-color: lighten($color: grey, $amount: 45);
+            transition: 0.2s all ease-in;
+        }
+        .entry-title {
+            font-size: 12px;
+        }
+    }
 </style>
