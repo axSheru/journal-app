9ac09a53d01f339f6b4a4c6fb8e50f47499eca0b
Alex Paredes <alexparedesr95@gmail.com>
2021-09-09 00:47:45 -0500

    FEAT: Se define la estructura base del componente EntryList así como sus estilos.

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>EntryList</h1>
+  <div class="entry-list-container">
+      <div class="px-2 pt-2">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Buscar entrada"
+          />
+      </div>
+      <div class="entry-scrollarea">
+          <h2
+            v-for="item in 100"
+            :key="item"
+          >
+              Entry
+          </h2>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +23,14 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+    .entry-list-container {
+        border-right: 1px solid #2c3e50;
+        height: calc( 100vh - 56px );
+    }
+    .entry-scrollarea {
+        height: calc( 100vh - 120px );
+        overflow: scroll;
+    }
+</style>
