4029cb5f064119cd56a5f7e970c8f1e6d0efed6f
Alex Paredes <alexparedesr95@gmail.com>
2021-09-10 01:36:35 -0500

    FEAT: Se añaden estilos al componente Fab.

--- src/modules/daybook/components/Fab.vue
+++ src/modules/daybook/components/Fab.vue
@@ -10,6 +10,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+    button {
+        border-radius: 100%;
+        bottom: 20px;
+        height: 70px;
+        position: fixed;
+        right: 20px;
+        width: 70px;
+    }
 
 </style>
