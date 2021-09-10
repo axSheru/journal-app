566f51302a674190c9d79db2767fddf0162198ab
Alex Paredes <alexparedesr95@gmail.com>
2021-09-10 01:37:07 -0500

    FEAT: Se importa el componente Fab en el componente NoEntrySelected.

--- src/modules/daybook/views/NoEntrySelected.vue
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -2,7 +2,18 @@
   <div class="d-flex justify-content-center">
       <h1 class="align-self-center">No hay nada seleccionado.</h1>
   </div>
+
+  <Fab />
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    components: {
+        Fab: defineAsyncComponent( () => import('../components/Fab.vue') )
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 
