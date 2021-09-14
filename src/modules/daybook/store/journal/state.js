785b534621f907aecc13afa2fe8eebce2f26e873
Alex Paredes <alexparedesr95@gmail.com>
2021-09-13 22:38:29 -0500

    FEAT: Se crean entradas del diario estáticas.

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,3 +1,23 @@
 export default () => ({
     isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'In culpa sunt proident nulla dolor commodo velit mollit minim.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Laboris quis aliquip ipsum ipsum anim qui.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Ad et aliqua dolor sit eiusmod elit.',
+            picture: null
+        },
+    ]
 })
