c0a1fe2140fe17c1d0a67ab2eeb4cfa5ed96137d
Alex Paredes <alexparedesr95@gmail.com>
2021-09-14 23:04:41 -0500

    CHORE: Se cambian ids en el state de journal.

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,19 +2,19 @@ export default () => ({
     isLoading: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'In culpa sunt proident nulla dolor commodo velit mollit minim.',
             picture: null
         },
         {
-            id: new Date().getTime() + 1000,
+            id: '2',
             date: new Date().toDateString(),
             text: 'Laboris quis aliquip ipsum ipsum anim qui.',
             picture: null
         },
         {
-            id: new Date().getTime() + 2000,
+            id: '3',
             date: new Date().toDateString(),
             text: 'Ad et aliqua dolor sit eiusmod elit.',
             picture: null
