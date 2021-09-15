a30f05f208b1ea90eba0597987a8ff19bbe1d0cd
Alex Paredes <alexparedesr95@gmail.com>
2021-09-14 22:30:26 -0500

    FEAT: Se añade la capacidad de filtrar por término en el journal->getter->getEntriesByTerm.

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -2,7 +2,11 @@
     return state;
 } */
 
-export const getEntriesByTerm = ( /*state*/ ) => {
+export const getEntriesByTerm = ( state ) => ( term = '' ) => {
+
+    if( term.length === 0 ) return state.entries
+
+    return state.entries.filter( entry => entry.text.toLowerCase().includes( term.toLocaleLowerCase() ) )
     
 }
 
