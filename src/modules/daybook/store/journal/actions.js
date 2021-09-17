93aa9b59212b940f90c9886c68f762ba735f69be
Alex Paredes <alexparedesr95@gmail.com>
2021-09-16 21:46:15 -0500

    FEAT: Se crea mutation setEntries para actualizar el state con lo que se recibe de firebase y se consume desde las actions.

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -4,10 +4,18 @@ import journalApi from '@/api/journalApi';
     
 } */
 
-export const loadEntries = async (/*{ commit }*/) => {
+export const loadEntries = async ({ commit }) => {
 
     const { data } = await journalApi.get('/entries.json')
-    console.log(data)
+    const entries = []
+    for( let id of Object.keys( data ) ) {
+        entries.push({
+            id,
+            ...data[id]
+        })
+    }
+
+    commit( 'setEntries', entries )
 
 }
 
