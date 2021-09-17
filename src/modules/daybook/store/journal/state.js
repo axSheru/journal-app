a23f85d1696e16f0542769d7f20bf0d820301cca
Alex Paredes <alexparedesr95@gmail.com>
2021-09-16 21:34:01 -0500

    REFACTOR: Se eliminan entradas default del state.

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text: 'In culpa sunt proident nulla dolor commodo velit mollit minim.',
-            picture: null
-        },
-        {
-            id: '2',
-            date: new Date().toDateString(),
-            text: 'Laboris quis aliquip ipsum ipsum anim qui.',
-            picture: null
-        },
-        {
-            id: '3',
-            date: new Date().toDateString(),
-            text: 'Ad et aliqua dolor sit eiusmod elit.',
-            picture: null
-        },
-    ]
+    entries: []
 })
