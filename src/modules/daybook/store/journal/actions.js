2c332a1b48b712f81b19b18b68af8108b6139a77
Alex Paredes <alexparedesr95@gmail.com>
2021-09-16 21:27:03 -0500

    FEAT: Se importa journalApi y se realiza petición para obtener las entradas.

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,9 +1,14 @@
+import journalApi from '@/api/journalApi';
+
 /* export const myAction = async ({ commit }) => {
     
 } */
 
 export const loadEntries = async (/*{ commit }*/) => {
-    
+
+    const { data } = await journalApi.get('/entries.json')
+    console.log(data)
+
 }
 
 export const updateEntry = async (/*{ commit }*/) => {
