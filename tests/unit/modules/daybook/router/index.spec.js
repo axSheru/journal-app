6b829bc8cc63bbdefca9cbdcecc6b75a5e619c73
Alex Paredes <alexparedesr95@gmail.com>
2021-09-23 00:48:52 -0500

    TEST: Se añade prueba para verificar que se retorne correctamente el id de la ruta.

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -26,8 +26,8 @@ describe('Pruebas en el router module del daybook.', () => {
 
     test('Debe de comprobar que las rutas te redirigen a los componentes esperados.', async () => {
         
-        //expect( (await daybookRouter.children[0].component()).default.name ).toBe('NoEntrySelected')
-        //expect( (await daybookRouter.children[1].component()).default.name ).toBe('EntryView')
+        //expect( (await daybookRouter.children[0].component()).default.name ).toBe('NoEntrySelected')//Muy rígido.
+        //expect( (await daybookRouter.children[1].component()).default.name ).toBe('EntryView')//Muy rígido.
 
         const promiseRoutes = []
         daybookRouter.children.forEach( child => promiseRoutes.push( child.component() ) )
@@ -36,6 +36,22 @@ describe('Pruebas en el router module del daybook.', () => {
 
         expect( routes ).toContain( 'EntryView' )
         expect( routes ).toContain( 'NoEntrySelected' )
+
+    })
+
+    test('Debe de retornar el id de la ruta.', () => {
+        
+        const route = {
+            params: {
+                id: 'ABC-123'
+            }
+        }
+
+        //expect( daybookRouter.children[1].props( route ) ).toEqual({ id: 'ABC-123' })//Muy rígido.
+
+        const entryRoute = daybookRouter.children.find( route => route.name === 'entry' )//Más dinámico.
+        
+        expect( entryRoute.props( route ) ).toEqual({ id: 'ABC-123' })
 
     })
 
