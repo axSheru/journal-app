e57e5048e7ecf03726842b36e8b3cf077f12ddcf
Alex Paredes <alexparedesr95@gmail.com>
2021-09-24 23:00:06 -0500

    TEST: Se añade prueba sobre mutation - setEntries.

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -24,5 +24,21 @@ describe('Vuex - Pruebas en el Journal Module', () => {
         expect( entries ).toEqual( journalState.entries )
         
     })
+
+    //Mutations.
+    test('mutation: setEntries', () => {
+        
+        const store = createVuexStore({ isLoading: true, entries: [] })
+
+        store.commit( 'journal/setEntries', journalState.entries )
+        expect( store.state.journal.entries.length ).toBe( 2 )
+        
+        store.commit( 'journal/setEntries', journalState.entries )
+        expect( store.state.journal.entries.length ).toBe( 4 )
+
+        expect( store.state.journal.isLoading ).toBeFalsy()
+
+    })
+    
     
 })
