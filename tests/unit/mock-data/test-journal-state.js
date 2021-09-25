e645d5f3d2c6d4e1459a675330019065b6f65981
Alex Paredes <alexparedesr95@gmail.com>
2021-09-24 22:44:04 -0500

    TEST: Se crea mock del state initial con datos.

---
+++ tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,17 @@
+export const journalState = {
+    isLoading: false,
+    entries: [
+        {
+            id: '-MjrZSR1hwEC8mfMROiA',
+            date: 1631946119673,
+            picture: "https://res.cloudinary.com/axsheru/image/upload/v1632117386/bchmkui2wgfjnqob7ahb.jpg",
+            text: "(DEVICE BEEPING)\n\n- (GRUNTS)\n- (TRAWLER LAUGHS)\n\n(SPEAKING HUTTESE)\n\n(IN ENGLISH) Please. Please.\n\n- I have credits. Take them.\n- (SPEAKING HUTTESE)\n\n- (DOOR OPENS)\n- (MYTHROL GASPS)\n\n(WIND BLOWING)\n\n(ALL SPEAKING HUTTESE)\n\n(INDISTINCT CHATTER)\n\n(SCOFFS)\n\nHe says you spilled his drink.\n\n\n\n(GRUNTS, PANTING)\n\nIt's fine.\n\n- It's on me.\n- (SPEAKING HUTTESE)\n\n(GROWLING)\n\nBARTENDER: Here.\n\n(ALL GRUNTING)\n\n(MAN GROANING)\n\n(BODY THUDS)\n\n(GRUNTING)\n\n(SCREAMS)\n\n(FLESH SQUELCHES)\n\n(INDISTINCT MURMURING)\n\n(MYTHROL LAUGHING)\n\nThank you! Thank you very much!"
+        },
+        {
+            id: '-Mk0mLXDm4o8EaMD_KGq',
+            date: 1632117541303,
+            picture: "https://res.cloudinary.com/axsheru/image/upload/v1632117719/bwes4j1uyefjt1yobyr1.jpg",
+            text: "Alive.\n\nYes. Alive.\n\nAlthough, I acknowledge\n\nthat bounty hunting\nis a complicated profession.\n\nThis being the case,\n\nproof of termination is also acceptable\nfor a lower fee.\n\nThat is not what we agreed upon.\n\nI'm simply being pragmatic.\n\nTHE MANDALORIAN: Let's see the puck.\n\nI'm afraid discretion dictates\n\na less traditional agreement.\n\nWe can only offer you a tracking fob.\n\nWhat's the chain code?\n\nWe can only provide the last four digits.\n\nTheir age? That's all you can give me?\n\nYes. They're 50 years old.\n\nWe can also give you\nlast reported positional data.\n\nBetween that and the fob,\n\na man of your skill\nshould make short work of this.\n\nThe Beskar belongs back\ninto the hands of a Mandalorian.\n\nIt is good to restore\nthe natural order of things\n\nafter a period of such disarray,\n\ndon't you agree?\n\n(INDISTINCT CHATTER)\n\n(ALIEN WHINING)\n\n(INDISTINCT CHATTER)\n\n(VENDOR YELLING IN HUTTESE)\n\n(CHILDREN GIGGLING)"
+        }
+    ]
+}
