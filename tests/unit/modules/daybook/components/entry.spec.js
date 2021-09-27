65fffd5e5142a592f528e63764772055a2e421f5
Alex Paredes <alexparedesr95@gmail.com>
2021-09-26 20:32:01 -0500

    TEST: Se prepara el archivo de tests para el componente Entry.

---
+++ tests/unit/modules/daybook/components/entry.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from '@vue/test-utils'
+import Entry from '@/modules/daybook/components/Entry'
+import { journalState } from '../../../mock-data/test-journal-state'
+
+describe('Pruebas en el entry component.', () => {
+
+    const mockRouter = {
+        push: jest.fn()
+    }
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallowMount( Entry, {
+            props: {
+                entry: journalState.entries[0]
+            },
+            global: {
+                mocks: {
+                    $router: mockRouter
+                }
+            }
+        })
+    });
+    
+})
