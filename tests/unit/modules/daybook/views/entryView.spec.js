595d83b39e14c05fe5ecd7d724456b57ddc3d2f0
Alex Paredes <alexparedesr95@gmail.com>
2021-09-26 23:48:02 -0500

    TEST: Se añade el último test de EntryView. Debe de borrar la entrada y salir. Actualmente tiene un error, ya publiqué la pregunta en la clase.

--- tests/unit/modules/daybook/views/entryView.spec.js
+++ tests/unit/modules/daybook/views/entryView.spec.js
@@ -1,6 +1,8 @@
 import { shallowMount } from '@vue/test-utils'
 import { createStore } from 'vuex'
 
+import Swal from 'sweetalert2'
+
 import journal from '@/modules/daybook/store/journal'
 import EntryView from '@/modules/daybook/views/EntryView'
 
@@ -16,9 +18,16 @@ const createVuexStore = ( initialState ) =>
         }
     })
 
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
+    showLoadig: jest.fn(),
+    close: jest.fn()
+}))
+
 describe('Pruebas en el EntryView', () => {
 
     const store = createVuexStore( journalState )
+    store.dispatch = jest.fn()
     const mockRouter = {
         push: jest.fn()
     }
@@ -62,6 +71,24 @@ describe('Pruebas en el EntryView', () => {
         
         expect( wrapper.html() ).toMatchSnapshot()
         expect( mockRouter.push ).not.toHaveBeenCalled()
+
+    })
+    
+    test('Debe de borrar la entrada y salir.', async() => {
+
+        Swal.fire.mockReturnValueOnce( Promise.resolve({ isConfirmed: true }) )
+        
+        await wrapper.find('.btn-danger').trigger('click')
+
+        expect( Swal.fire ).toHaveBeenCalledWith({
+            title: '¿Está seguro?',
+            text: 'Una vez borrado no se puede recuperar.',
+            showDenyButton: true,
+            confirmButtonText: 'Sí, estoy seguro.'
+        })
+
+        expect( store.dispatch ).toHaveBeenCalledWith('journal/deleteEntry', '-MjrZSR1hwEC8mfMROiA')
+        expect( mockRouter.push ).toHaveBeenCalled()
 
     })
     
